0624e73849c64038e7661b99ede3245c03c87b5c
이은지/(협력사) 대리/클라우드플랫폼(연) 클라우드플랫폼개발팀(eunjitech.lee@lgepartner.com) <>
2022-01-14 09:44:53 +0900

    1.0 firebase setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import app from './firebase';
+
+console.log(app);
 
 ReactDOM.render(
   <React.StrictMode>
